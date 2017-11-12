06029247ac0fc8fc8e9376c70238f4a435434498
Ken Vilar <kenvilar@gmail.com>
2017-11-13 02:19:10 +0800

    Fix conditional statement and confirming similarity check

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def translate(myWord):
 	if myWord in data:
 		return data[myWord]
 	elif len(get_match_keyword(myWord, data.keys())) > 0:
-		return 'Did you mean %s instead?' % get_match_keyword(myWord, data.keys())[0]
+		best_match = get_match_keyword(myWord, data.keys())[0]
+		yesOrNo = input('Did you mean %s instead? Please enter Y if yes, or N if no. \n' % best_match)
+		if yesOrNo == 'Y':
+			correctWord = get_match_keyword(myWord, data.keys())[0]
+			return data[correctWord]
+		elif yesOrNo == 'N':
+			return 'Your keywords doesn\'t exist.'
+		else:
+			return 'Just Y or N only'
 	else:
 		return 'Your keywords doesn\'t exist.'
 
