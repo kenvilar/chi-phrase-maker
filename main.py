36b07a3c65585793b448121274e019b78c595a97
Ken Vilar <kenvilar@gmail.com>
2017-11-12 23:37:08 +0800

    Change the "word" to "myWord" to fix its shadow name defined in outer scope

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 data = json.load(open('phrases.json', 'r'))
 
 
-def translate(word):
-	if word in data:
-		return data[word]
+def translate(myWord):
+	if myWord in data:
+		return data[myWord]
 	else:
 		return 'Your keywords doesn\'t exist.'
 
 
-word = input('Enter your keyword/s: ')
-translate = translate(word)
+myWord = input('Enter your keyword/s: ')
+translate = translate(myWord)
 print(translate)
