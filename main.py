07a85992dd37cedce2cb0735a5f29bad319d2215
Ken Vilar <kenvilar@gmail.com>
2017-11-12 22:36:49 +0800

    Implement if input keyword is in the data

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import json
 
 data = json.load(open('phrases.json', 'r'))
-print(data)
+
+
+def translate(word):
+	if word in data:
+		print(data[word])
+	else:
+		print('Your keywords doesn\'t exist.')
+
+
+translate('Hello')
